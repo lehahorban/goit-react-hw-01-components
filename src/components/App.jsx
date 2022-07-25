9e66498970974f3ff63affecdb4efd67e9e5977e
Oleksii Horban <lehagorban93@gmail.com>
2022-07-25 14:28:39 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,24 @@
-import Profile from "./Profile/Profile"
-import Statistics from "./Statistics/Statistics"
-import FriendList from "./FriendList/FriendList"
-import TransactionHistory from "./TransactionHistory/TransactionHistory"
-import user from './Profile/user'
-import data from "./Statistics/data"
-import friends from "./FriendList/friends"
-import transactions from "./TransactionHistory/transactions"
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import user from './Profile/user';
+import data from './Statistics/data';
+import friends from './FriendList/friends';
+import transactions from './TransactionHistory/transactions';
 export const App = () => {
   return (
     <div className="App">
-     <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />      
-      <FriendList friends={friends} />      
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
-     
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,22 @@
 import PropTypes from 'prop-types';
-import style from "./Statistics.module.css"
+import style from './Statistics.module.css';
 
 function Statistics({ title, stats }) {
-     return (
-        <section className={style.statistics}>
-  <h2 className={style.title}>Upload stats</h2>
+  return (
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title}</h2>}
 
-  <ul className={style.statList}>
-    {stats.map(item => <li key = {item.id} className={style.item}>
-                    <span className={style.label}>{ item.label}</span>
-      <span className={style.percentage}>{item.percentage}%</span>
-    </li>  ) } 
-  </ul>
-</section>
-        
-      
-        
-    )
+      <ul className={style.statList}>
+        {stats.map(item => (
+          <li key={item.id} className={style.item}>
+            <span className={style.label}>{item.label}</span>
+            <span className={style.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
-
 
 Statistics.propTypes = {
   stats: PropTypes.arrayOf(
@@ -26,8 +24,8 @@ Statistics.propTypes = {
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
-  })
-)
-}
+    })
+  ),
+};
 
-export default Statistics
+export default Statistics;
